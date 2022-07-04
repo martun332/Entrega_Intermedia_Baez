c11dca239dae6fc1b459af7246375f74894ca1aa
Martin Baez <martin.baez@live.com.ar>
2022-07-04 19:15:31 -0300

    solucionando el NOT NULL del modelo

--- app_proyecto/models.py
+++ app_proyecto/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class familiares(models.Model):
-    nombre = models.CharField(max_length=30)
-    email = models.EmailField()
-    edad = models.IntegerField()
+    nombre = models.CharField(max_length=30, null=True)
+    email = models.EmailField(null=True)
+    edad = models.IntegerField(null=True)
     fecha_nacimiento = models.DateField(null=True)
-    creacion = models.DateField(null=True)
+    creacion = models.DateTimeField(auto_now_add=True, blank=True)
